7c34b1224540aabc51cc7c553a23080edef11731
tongplw <bangkok.tong@gmail.com>
2020-03-30 13:06:23 +0700

    edit exercise 3 cout

--- Lab05/Lab05.srcs/sources_1/imports/Lab5Verilog/alu.v
+++ Lab05/Lab05.srcs/sources_1/imports/Lab5Verilog/alu.v
@@ -6,7 +6,7 @@
 // Purpose      : Computer Architecture
 // Developers   : Krerk Piromsopa, Ph. D.
 //              : Chulalongkorn University.
-module alu(S,z,Cout,A,B,Cin,alu_ops);
+module alu(S, z, Cout, A, B, Cin, alu_ops);
 output reg [31:0] S;
 output z;
 output reg Cout;
@@ -15,7 +15,7 @@ input [31:0] B;
 input Cin;
 input [2:0] alu_ops;
 
-assign z=~|S;
+assign z = ~|S;
 
 always @(A or B or alu_ops)
 begin
@@ -25,9 +25,9 @@ begin
     3'b101: begin S = -A; Cout = 0; end
 	3'b100: begin S = A ^ B; Cout = 0; end
 	3'b011: begin S = A & B; Cout = 0; end
-	3'b010: begin S=A|B; Cout=0; end
-	3'b001: {Cout,S}=A-B;
-	default: {Cout,S}=A+B+Cin;
+	3'b010: begin S = A | B; Cout = 0; end
+	3'b001: {Cout, S} = A - B;
+	default: {Cout, S} = A + B + Cin;
     endcase
 end
 
--- Lab05/Lab05.srcs/sources_1/new/alu_test.v
+++ Lab05/Lab05.srcs/sources_1/new/alu_test.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ns
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/08/2019 02:00:20 PM
-// Design Name: 
-// Module Name: alu_test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu_test();
     wire [31:0] S;
